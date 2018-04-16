a7d0f4ffe7af0e4e563a4acfe21851fd2ad41f3e
andrewbackes <rbackes@ucla.edu>
2018-04-16 16:27:27 -0700

    Jenkins fix

--- scripts/publish-deb.sh
+++ scripts/publish-deb.sh
@@ -2,7 +2,7 @@
 
 GIT_HASH=$(git rev-parse HEAD | cut -c -7)
 VERSION=0.${BUILD_NUMBER}.0-h${BUILD_NUMBER}.${GIT_HASH}
-DEB_FILE=canary-test_${VERSION}_all.deb
+DEB_FILE=canary-tester_${VERSION}_all.deb
 METADATA="deb_distribution=trusty;deb_component=main;publish=1;deb_architecture=i386,amd64"
 
 REPO="debians-dev"
