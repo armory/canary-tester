e21480f01f3a64cf1ec1ffcf5c77e2a3dd316347
andrewbackes <rbackes@ucla.edu>
2018-04-17 15:47:23 -0700

    Randomly output success/failure

--- cmd/canary-tester.go
+++ cmd/canary-tester.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"net/http"
-	"os"
 	"time"
 
 	"github.com/DataDog/datadog-go/statsd"
@@ -11,50 +11,52 @@ import (
 
 func main() {
 	fmt.Println("Canary Tester started.")
-	c, _ := statsd.New("127.0.0.1:8125")
-	c.Namespace = "canarytester."
-	fail := shouldFail()
-	http.HandleFunc("/fail/true", func(w http.ResponseWriter, r *http.Request) {
-		fail = true
-		fmt.Println("All responses from now on will be 200.")
-		fmt.Println("Received", r)
-	})
-	http.HandleFunc("/fail/false", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("Received", r)
-		fmt.Println("All responses from now on will be 500 errors.")
-		fail = false
-	})
+	failureRate := 0.10
+	setRootHandlers()
+	setToggleHandlers(&failureRate)
+	go dataDogPulse(&failureRate)
+	http.ListenAndServe(":8080", nil)
+	fmt.Println("Done.")
+}
+
+func setRootHandlers() {
 	http.HandleFunc("/healthcheck", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("Received", r)
 		w.WriteHeader(200)
 	})
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("Received", r)
-		if fail {
-			c.Incr("http_500", nil, 1)
-			w.WriteHeader(500)
-		} else {
-			c.Incr("http_200", nil, 1)
-			w.WriteHeader(200)
-		}
+		w.Write([]byte(`{"status":"ok"`))
 	})
-	go heartBeat()
-	http.ListenAndServe(":8080", nil)
-	fmt.Println("Done.")
 }
 
-func heartBeat() {
+func setToggleHandlers(failureRate *float64) {
+	http.HandleFunc("/fail/true", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("Emitting more failures.")
+		fmt.Println("Received", r)
+		*failureRate = 0.90
+	})
+	http.HandleFunc("/fail/false", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("Emitting more successes.")
+		fmt.Println("Received", r)
+		*failureRate = 0.10
+	})
+}
+
+func dataDogPulse(failureRate *float64) {
+	c, _ := statsd.New("127.0.0.1:8125")
+	c.Namespace = "canarytester."
 	for {
-		resp, _ := http.Get("http://localhost:8080/")
-		resp.Body.Close()
-		time.Sleep(10 * time.Second)
+		r := rand.Float64()
+		name := "success"
+		if r < *failureRate {
+			name = "failure"
+		}
+		fmt.Println(name)
+		err := c.Incr(name, nil, 1.0)
+		if err != nil {
+			fmt.Println(err)
+		}
+		time.Sleep(1 * time.Second)
 	}
-}
-
-func shouldFail() bool {
-	f := os.Getenv("RETURN_500")
-	if f == "true" {
-		return true
-	}
-	return false
 }
